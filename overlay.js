4d91f4c4aaa73926d486c899abb8bbaf88e40f54
Océane <bocnee.aurel@gmail.com>
2024-09-04 20:21:33 +0200

    :adhesive_bandage: Delete test

--- overlay.js
+++ overlay.js
@@ -1,41 +1,26 @@
 const chatContainer = document.getElementById("chat-container");
 
-function addMessage(user, message, color) {
-  if (user === "Nightbot") {
-    return;
-  } else {
-    const messageId = `message-${Date.now()}`;
-    const messageElement = `
-                    <div id="${messageId}" class="message">
-                        <span style="color: ${
-                          color || "#000"
-                        };">${user}: </span>
-                        ${message}
-                    </div>
-                `;
-    chatContainer.insertAdjacentHTML("beforeend", messageElement);
-    chatContainer.scrollTop = chatContainer.scrollHeight;
-
-    setTimeout(() => {
-      const elementToRemove = document.getElementById(messageId);
-      if (elementToRemove) {
-        elementToRemove.classList.add("fade-out");
-        setTimeout(() => {
-          if (elementToRemove) {
-            chatContainer.removeChild(elementToRemove);
-          }
-        }, 500);
-      }
-    }, 100000);
-  }
-}
-
 function getChannelNameFromUrl() {
   const params = new URLSearchParams(window.location.search);
   return params.get("channel");
 }
 
-// Initialize ComfyJS with the channel name from the URL
+function addMessage(user, message, color) {
+  const messageElement = `
+    <div class="message">
+      <span style="color: ${color || "#000"};">${user}: </span>
+      ${message}
+    </div>
+  `;
+  chatContainer.insertAdjacentHTML("beforeend", messageElement);
+  chatContainer.scrollLeft = chatContainer.scrollWidth;
+}
+
+ComfyJS.onChat = (user, message, flags, self, extra) => {
+  const color = extra.userColor || "#000";
+  addMessage(user, message, color);
+};
+
 const twitchChannel = getChannelNameFromUrl();
 if (twitchChannel) {
   ComfyJS.onChat = (user, message, flags, self, extra) => {
@@ -47,7 +32,9 @@ if (twitchChannel) {
   console.error("No Twitch channel name provided in the URL.");
 }
 
-addMessage("ZeldaFan92", "Trop hâte de voir le prochain boss !", "#8A2BE2");
+// À décommenter pour faire des tests fictifs
+
+/* addMessage("ZeldaFan92", "Trop hâte de voir le prochain boss !", "#8A2BE2");
 addMessage(
   "GamerDudeX",
   "Est-ce que quelqu'un a déjà essayé cette stratégie ?",
@@ -64,4 +51,4 @@ addMessage(
 );
 addMessage("EpicGamerGirl", "On va écraser cette équipe !", "#FF1493");
 addMessage("NoobMaster", "Comment on fait déjà ce combo ?", "#6A5ACD");
-addMessage("CaféRageux", "Cette partie me rend dingue !", "#DC143C");
+addMessage("CaféRageux", "Cette partie me rend dingue !", "#DC143C"); */
